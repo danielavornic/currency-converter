4cf44b05c816614469739e73220bdbe041342f08
Daniela <daniela.vornic@gmail.com>
2020-07-25 17:00:58 +0300

    implement conversion function in both ways

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 $('document').ready(function() {
-    function getRates(base, rate) {
+    getRates();
+
+    function getRates(reverse) {
+        if (reverse) {
+            var fromCurrency = $('#toSection .options select').val();
+            var toCurrency = $('#fromSection .options select').val();
+        } else {
+            var fromCurrency = $('#fromSection .options select').val();
+            var toCurrency = $('#toSection .options select').val();
+        }
+
         endpoint = 'latest';
         access_key = '7803ae90ffab404bd0fa8c957abd2b0a';
-
         $.ajax({
-            url: 'http://data.fixer.io/api/' + endpoint + '?access_key=' + access_key + '&base=' + base,   
+            url: 'http://data.fixer.io/api/' + endpoint + '?access_key=' + access_key,   
             dataType: 'json',
             success: function(json) {
-                console.log(json.rates);
+                base = json.rates[fromCurrency];
+                rate = json.rates[toCurrency];
+                if (reverse) {
+                    reverseResult(base, rate)
+                } else {
+                    result(base, rate)
+                }
             }
         });
     }
 
-    getRates('USD', 'EUR');
-
-    $('input').toArray().forEach(function(field) {
-        new Cleave(field, {
-            numericOnly: true,
-            numeral: true,
-            numeralThousandsGroupStyle: 'thousand'
+    function result(base, rate) {
+        var amount = Number($('#amountFrom').val().replace(/,/g , ''));
+        var result = amount / base * rate;
+        $('#amountTo').val(result.toFixed(2));
+        $('input').toArray().forEach(function(field) {
+            new Cleave(field, {
+                numeral: true,
+                numeralThousandsGroupStyle: 'thousand'
+            });
         });
-    });
+    }
+
+    function reverseResult(base, rate) {
+        var amount = Number($('#amountTo').val().replace(/,/g , ''));
+        var result = amount / base * rate;
+        $('#amountFrom').val(result.toFixed(2));
+    }
+
+    $('#amountFrom').keyup(function() {getRates()})
+    $('#amountTo').keyup(function() {getRates(true)})
+    $('.options select').change(function() {getRates()})
 
 })
